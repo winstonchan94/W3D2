f844febc3965612c1b4860e8d5bc76cfa216479b
Brie McNally <brienne.mcnally@gmail.com>
2018-02-27 18:06:30 -0800

    Go as far as we can

--- aA/model_base.rb
+++ aA/model_base.rb
@@ -43,7 +43,66 @@ class ModelBase
     FROM
       #{database}
     SQL
-    
+
+    data.map {|datum| self.new(datum)}
+  end
+
+  def save
+    vars = self.instance_variables.map(&:to_s)
+    set_string = ""
+    vars.each do |var|
+      next if var == '@id'
+        set_string += var[1..-1] + " = ?,\n"
+    end
+    set_string = set_string[0..-3]
+
+    vars_string = self.instance_variables.join(', ').to_s
+    raise "#{self} not in database" unless @id
+    QuestionsDatabase.instance.execute(<<-SQL, vars_string)
+      UPDATE
+        #{DATABASES[self]}
+      SET
+        #{set_string}
+      WHERE
+        id = ?
+    SQL
+
+  end
+
+  def self.where(options)
+    arguments = options.sort
+    num_arg = arguments.length
+    arguments.flatten!
+    arguments.map!(&:to_s)
+    vars_string = arguments.join(', ').to_s
+    where_array = []
+    num_arg.times { where_array << '? = ?'}
+    where_string = where_array.join('AND ')
+    p where_string
+    p vars_string
+    # heredoc = <<-SQL, *arguments
+    #   SELECT
+    #     *
+    #   FROM
+    #     #{DATABASES[self]}
+    #   WHERE
+    #     #{where_string}
+    #   SQL
+    #   p heredoc
+
+
+
+    data = QuestionsDatabase.instance.execute(<<-SQL, *arguments)
+      SELECT
+        *
+      FROM
+        #{DATABASES[self]}
+      WHERE
+        #{where_string}
+
+
+      SQL
+    p data
     data.map {|datum| self.new(datum)}
   end
 
